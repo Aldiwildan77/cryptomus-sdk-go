e5a1d55517a4aae6fb56d1409cfe1b6bdd9b8dc2
aldiwildan77 <aldiwild77@gmail.com>
2025-02-17 09:28:45 +0700

    feat: add balance sdk

---
+++ balance.go
@@ -0,0 +1,45 @@
+package cryptomus_sdk_go
+
+import "context"
+
+type BalanceResponse struct {
+	*HTTPResponse
+	Result []BalanceResult `json:"result,omitempty"`
+}
+
+type Merchant struct {
+	UUID         string `json:"uuid"`
+	Balance      string `json:"balance"`
+	CurrencyCode string `json:"currency_code"`
+}
+
+type User struct {
+	UUID         string `json:"uuid"`
+	Balance      string `json:"balance"`
+	CurrencyCode string `json:"currency_code"`
+}
+
+type Balance struct {
+	Merchant []Merchant `json:"merchant"`
+	User     []User     `json:"user"`
+}
+
+type BalanceResult struct {
+	Balance Balance `json:"balance"`
+}
+
+func (sdk *Cryptomus) Balance(ctx context.Context) (*BalanceResponse, error) {
+	var result BalanceResponse
+
+	req := sdk.HttpClient.NewRequest().
+		SetContext(ctx).
+		SetHeader("Content-Type", "application/json").
+		SetSuccessResult(&result).
+		SetErrorResult(&result)
+
+	if _, err := req.Get(BalanceEndpoint.URL()); err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
